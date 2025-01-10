81ced34ae52a3610dc2a24fc3ff8aa272b04e1b8
Spencer Salyer <spencer.salyer@proton.me>
2025-01-10 16:34:51 -0500

    idk what i doing pt 2 -- matrix woes

--- keyboards/handwired/b3rtsuran/keymaps/default/keymap.c
+++ keyboards/handwired/b3rtsuran/keymaps/default/keymap.c
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_BL] = LAYOUT_buran(
 
         KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,                       KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,
-        KC_A,        KC_S,        KC_D,        KC_F,        KC_G,                       KC_H,        KC_J,        KC_K,        LT(_FFL,KC_L),  KC_SCOLON,
+        KC_A,        KC_S,        KC_D,        KC_F,        KC_G,                       KC_H,        KC_J,        KC_K,        LT(_FFL,KC_L),  KC_SCLN,
         KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,                       KC_N,        KC_M,        KC_COMMA,    KC_DOT,        KC_SLASH,
-        KC_LCTRL,    MO(_FL),     KC_LGUI,     LT(_BL,KC_SPACE),                        KC_BSPACE,   KC_RALT,     MO(1),       KC_LSHIFT
+        KC_LCTL,    MO(_FL),     KC_LGUI,     LT(_BL,KC_SPACE),                         KC_BSPC,   KC_RALT,     MO(1),       KC_LSFT
 
     ),
 /* b3rtsuran
