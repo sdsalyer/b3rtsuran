b332ad71ebe58410c8a8c4a2880910a521e9d8c6
Spencer Salyer <spencer.salyer@proton.me>
2025-01-11 10:49:22 -0500

    made 40 key matrix trying to simplify things, can still only use one side at a time, though

--- keyboards/handwired/b3rtsuran/keymaps/default/keymap.c
+++ keyboards/handwired/b3rtsuran/keymaps/default/keymap.c
@@ -35,12 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  └────┴────┴─────┘└──────┘ └────┘ └──────┘└─────┴────┴────┘
    */
     // TODO: Encoder - ? KC_MPLY
-   [_BL] = LAYOUT_buran(
+   [_BL] = LAYOUT(
 
         KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,                       KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,
         KC_A,        KC_S,        KC_D,        KC_F,        KC_G,                       KC_H,        KC_J,        KC_K,        LT(_FFL,KC_L),  KC_SCLN,
         KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,                       KC_N,        KC_M,        KC_COMMA,    KC_DOT,        KC_SLASH,
-        KC_LCTL,    MO(_FL),     KC_LGUI,     LT(_BL,KC_SPACE),                         KC_BSPC,   KC_RALT,     MO(1),       KC_LSFT
+        KC_LCTL,    MO(_FL),     KC_LGUI,      KC_NO,       LT(_BL,KC_SPACE),           KC_BSPC,     KC_NO,       KC_RALT,     MO(1),       KC_LSFT
 
     ),
 /* b3rtsuran
@@ -55,12 +55,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  │lalt│    │     ││      │ │    │ │ del  ││     │    │    │
    *  └────┴────┴─────┘└──────┘ └────┘ └──────┘└─────┴────┴────┘
   */
-   [_FL] = LAYOUT_buran(
+   [_FL] = LAYOUT(
 
         KC_1,        KC_2,        KC_3,        KC_4,       KC_5,                         KC_6,        KC_7,      KC_8,        KC_9,        KC_0,
         KC_ESC,      KC_LBRC,     KC_RBRC,     KC_BSLS,    KC_QUOT,                      KC_LEFT,     KC_DOWN,   KC_UP,       KC_RIGHT,    KC_NO,
         KC_LSFT,     KC_NO,       KC_QUOT,     KC_MINS,    KC_EQL,                       KC_QUOT,     KC_NO,     LSFT(KC_2),  KC_GRV,      KC_SLASH,
-        KC_LALT,     KC_NO,       KC_NO,       KC_NO,                                    KC_DEL,      KC_NO,     KC_NO,       KC_NO
+        KC_LALT,     KC_NO,       KC_NO,       KC_NO,      KC_NO,                        KC_DEL,      KC_NO,     KC_NO,       KC_NO,       KC_NO
 
     ),
  /*b3rtsuran
@@ -75,12 +75,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  │VOLU│VOLD│MUTE ││BRIGHT│ │    │ │      ││     │    │    │
    *  └────┴────┴─────┘└──────┘ └────┘ └──────┘└─────┴────┴────┘
    */
-   [_FFL] = LAYOUT_buran(
+   [_FFL] = LAYOUT(
 
         KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_NO,                    KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
         KC_F5,        KC_F6,        KC_F7,        KC_F8,        KC_NO,                    KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
         KC_F9,        KC_F10,       KC_F11,       KC_F12,       KC_NO,                    KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
-        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                  KC_NO,        KC_NO,        KC_NO,        KC_NO
+        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                    KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
 
     )
 };
